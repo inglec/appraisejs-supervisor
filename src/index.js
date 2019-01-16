bdd83abfecabace65b17cba52288842709e28a2e
inglec <inglec@tcd.ie>
2019-01-16 18:40:51 +0000

    Begin webhook route

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const https = require('https');
 const jwt = require('jsonwebtoken');
 const path = require('path');
 // const shell = require('shelljs');
+const httpStatus = require('http-status-codes');
 
 const utils = require('./utils');
 
@@ -15,43 +16,29 @@ const hostname = '127.0.0.1';
 const port = 3000;
 const server = http.Server(app);
 
-const requestGitHubAccessToken = (appId, installationId) => {
-  const token = generateJWT(appId, path.join(__dirname, '../keys', 'private-key.pem'));
-
-  const options = {
+const requestAccessToken = (jwt, installationId) => {
+  return utils.httpRequestPromise({
     headers: {
       'Accept': 'application/vnd.github.machine-man-preview+json',
-      'Authorization': 'Bearer ' + token,
+      'Authorization': `Bearer ${jwt}`,
       'User-Agent': 'AppraiseJs'
     },
     hostname: 'api.github.com',
     method: 'POST',
     path: `/app/installations/${installationId}/access_tokens`
-  };
-
-  return new Promise((resolve, reject) => {
-    const req = https.request(options, (res) => {
-      var buffer = '';
-
-      res.on('data', (data) => {
-        buffer += data.toString();
-      });
-
-      res.on('error', err => reject(err));
-
-      res.on('end', () => resolve(JSON.parse(buffer)));
-    });
-
-    req.on('error', err => reject(err));
-
-    req.end();
   });
 };
 
-const processWebhook = (request) => {
-  requestGitHubAccessToken(process.env.APP_ID, request.repository.id)
-    .then(response => console.log(response))
-    .catch(error => console.log(error))
+const processPushWebhook = (payload) => {
+  console.log(payload);
+
+  const privateKeyPath = path.join(__dirname, '../keys', 'private-key.pem');
+  const jwt = utils.generateJWT(process.env.APP_ID, privateKeyPath);
+
+  requestAccessToken(jwt, payload.installation.id)
+    .then((response) => {
+      console.log(response);
+    })
 };
 
 const setupExpress = (app) => {
@@ -70,51 +57,27 @@ const setupExpress = (app) => {
     next();
   });
 
-  // Pre-flight requests.
-  app.options('*', (req, res) => res.send(200));
-}
-
-const setupEndpoints = (app) => {
+  // Routes.
   app.get('/', (req, res) => {
-    res.status(200);
-    res.json({ working: true });
+    res.status(httpStatus.OK);
+    res.send('Hello world');
     res.end();
   });
-
-  // Handle GitHub app user authorisation callback.
   app.get('/callback', (req, res) => {
-    res.status(200);
-    res.end();
-
-    processWebhook(req.body);
-  });
-
-  // Handle GitHub app webhook.
-  app.post('/webhook', (req, res) => {
-    res.status(200);
+    res.status(httpStatus.OK);
     res.end();
 
     console.log(req.body);
   });
-};
+  app.post('/webhook', (req, res) => {
+    res.status(httpStatus.OK);
+    res.end();
 
-const generateJWT = (appId, privateKeyPath) => {
-  const privateKey = fs.readFileSync(privateKeyPath);
-
-  // Get number of seconds since Epoch.
-  const time = Math.floor(new Date().getTime() / 1000);
-  const payload = {
-    iat: time, // Issued at time.
-    exp: time + (10 * 60), // Expiration time (10 minute maximum).
-    iss: appId
-  };
-
-  // Sign JSON Web Token and encode with RS256.
-  return jwt.sign(payload, privateKey, { algorithm: 'RS256' });
-};
+    processPushWebhook(req.body);
+  });
+}
 
 setupExpress(app);
-setupEndpoints(app);
 
 server.listen(port, hostname, (err) => {
   if (err) {
--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,48 @@
+const generateJWT = (appId, privateKeyPath) => {
+  const privateKey = fs.readFileSync(privateKeyPath);
+
+  // Get number of seconds since Epoch.
+  const time = Math.floor(new Date().getTime() / 1000);
+  const payload = {
+    iat: time, // Issued at time.
+    exp: time + (10 * 60), // Expiration time (10 minute maximum).
+    iss: appId
+  };
+
+  // Sign JSON Web Token and encode with RS256.
+  return jwt.sign(payload, privateKey, { algorithm: 'RS256' });
+};
+
+const httpRequestPromise = (options) => {
+  return new Promise((resolve, reject) => {
+    const req = https.request(options, (res) => {
+      var buffer = '';
+
+      res.on('data', (data) => {
+        buffer += data.toString();
+      });
+
+      res.on('error', err => reject(err));
+
+      res.on('end', () => resolve(JSON.parse(buffer)));
+    });
+
+    req.on('error', err => reject(err));
+
+    req.end();
+  });
+};
+
 const toHeaderField = array => array.join(', ');
 
 const toPrettyString = (string) => {
   const json = JSON.parse(string);
-  const pretty = JSON.stringify(json, null, 2);
-  return pretty;
+  return JSON.stringify(json, null, 2);
 };
 
-module.exports = { toHeaderField, toPrettyString };
+module.exports = {
+  generateJWT,
+  httpRequestPromise,
+  toHeaderField,
+  toPrettyString
+};
