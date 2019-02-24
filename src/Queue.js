efe8798c740167526b23f98cb31f91635d78beae
inglec <inglec@tcd.ie>
2019-02-24 19:03:19 +0000

    Queue: Add event listeners

--- src/Queue.js
+++ src/Queue.js
@@ -1,14 +1,54 @@
 class Queue {
   constructor() {
-    this.queue = [];
+    this.clear();
   }
 
-  enqueue(job) {
-    this.queue.push(job);
+  clear() {
+    this.queue = [];
+
+    if (typeof this.onChange === 'function') {
+      this.onChange([], 'clear');
+    }
+  }
+
+  enqueue(enqueued) {
+    this.queue.push(enqueued);
+
+    if (typeof this.onChange === 'function') {
+      this.onChange(enqueued, this.queue, 'enqueue');
+    }
+
+    if (typeof this.onEnqueue === 'function') {
+      this.onEnqueue(enqueued, this.queue);
+    }
+
+    return this.queue;
   }
 
   dequeue() {
-    return this.queue.shift();
+    const dequeued = this.queue.shift();
+
+    if (typeof this.onChange === 'function') {
+      this.onChange(dequeued, this.queue, 'dequeue');
+    }
+
+    if (typeof this.onDequeue === 'function') {
+      this.onDequeue(dequeued, this.queue);
+    }
+
+    return this.queue;
+  }
+
+  setOnChange(callback) {
+    this.onChange = callback;
+  }
+
+  setOnDequeue(callback) {
+    this.onDequeue = callback;
+  }
+
+  setOnEnqueue(callback) {
+    this.onEnqueue = callback;
   }
 }
 
