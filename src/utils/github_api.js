092ec39d251122550f6c791da95f606040a549dc
inglec <inglec@tcd.ie>
2019-02-25 14:42:41 +0000

    Github API: Reorder functions

--- src/utils/github_api.js
+++ src/utils/github_api.js
@@ -4,20 +4,6 @@ const GITHUB_API_URL = 'https://api.github.com';
 const GITHUB_APPS_MEDIA_TYPE = 'application/vnd.github.machine-man-preview+json';
 const GITHUB_URL = 'https://github.com';
 
-// Fetch OAuth access token for a given installation.
-const fetchInstallationAccessToken = (installationId, jwt, appName) => (
-  axios.request({
-    method: 'POST',
-    baseURL: GITHUB_API_URL,
-    url: `/app/installations/${installationId}/access_tokens`,
-    headers: {
-      Accept: GITHUB_APPS_MEDIA_TYPE,
-      Authorization: `Bearer ${jwt}`,
-      'User-Agent': appName,
-    },
-  })
-);
-
 // Exchange code for OAuth access token.
 const fetchClientAccessToken = (clientId, clientSecret, code) => (
   axios.request({
@@ -32,6 +18,20 @@ const fetchClientAccessToken = (clientId, clientSecret, code) => (
       client_id: clientId,
       client_secret: clientSecret,
       code,
+    },
+  })
+);
+
+// Fetch OAuth access token for a given installation.
+const fetchInstallationAccessToken = (installationId, jwt, appName) => (
+  axios.request({
+    method: 'POST',
+    baseURL: GITHUB_API_URL,
+    url: `/app/installations/${installationId}/access_tokens`,
+    headers: {
+      Accept: GITHUB_APPS_MEDIA_TYPE,
+      Authorization: `Bearer ${jwt}`,
+      'User-Agent': appName,
     },
   })
 );
